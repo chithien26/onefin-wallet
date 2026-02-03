9282b055927efea8148a1ec954c19488d52b3e4e
chithien26 <lechithien26@gmail.com>
2026-02-03 15:48:59 +0700

    Init project

---
+++ onefin-wallet/src/main/java/com/onefin/onefin_wallet/OneFinWalletApplication.java
@@ -0,0 +1,13 @@
+package com.onefin.onefin_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneFinWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneFinWalletApplication.class, args);
+	}
+
+}
---
+++ onefin-wallet/src/test/java/com/onefin/onefin_wallet/OneFinWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onefin.onefin_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneFinWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
