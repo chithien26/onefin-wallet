925bc6f93ccc2e3d291f9a5b4f54280fc554a70d
chithien26 <lechithien26@gmail.com>
2026-02-05 15:51:02 +0700

    add wallet

--- onefin-wallet/src/main/java/com/onefin/onefin_wallet/entity/wallet/MainWallet.java
+++ onefin-wallet/src/main/java/com/onefin/onefin_wallet/entity/wallet/MainWallet.java
@@ -4,13 +4,11 @@ import com.onefin.onefin_wallet.entity.base.BaseWallet;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @SuperBuilder
