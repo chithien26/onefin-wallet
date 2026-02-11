32b19640875614f495ed96c589e0c433603aa2bf
chithien26 <lechithien26@gmail.com>
2026-02-11 15:40:55 +0700

    validate transaction request

--- onefin-wallet/src/main/java/com/onefin/onefin_wallet/dto/request/TransferRequest.java
+++ onefin-wallet/src/main/java/com/onefin/onefin_wallet/dto/request/TransferRequest.java
@@ -1,5 +1,8 @@
 package com.onefin.onefin_wallet.dto.request;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +15,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 public class TransferRequest {
+    @NotNull
     String fromWallet;
+    @NotNull
     String toWallet;
+    @Min(value = 2000, message = "Amount must be at least 2000")
     BigDecimal amount;
     String description;
 
+    @AssertTrue(message = "From and To wallet must be different")
+    public boolean isDifferentAccount() {
+        return !fromWallet.equals(toWallet);
+    }
 }
